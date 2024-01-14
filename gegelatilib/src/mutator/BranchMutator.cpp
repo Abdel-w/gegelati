488a3b571f1b19c60ee79e28310d566b6d184341
Abdelouahed Drissi <92274694+Abdel-w@users.noreply.github.com>
2024-01-14 19:42:17 +0100

    Add FLAgent class that inherits from LearningAgent. Add a namespace (BranchMutator) that defines some methods used to copy a TPGGraph1 (which is an individual branch) into another TPGGraph2.

---
+++ gegelatilib/src/mutator/BranchMutator.cpp
@@ -0,0 +1,96 @@
+#include "tpg/tpgAction.h"
+#include "tpg/tpgEdge.h"
+#include "tpg/tpgGraph.h"
+#include "tpg/tpgTeam.h"
+
+#include "mutator/BranchMutator.h"
+#include "mutator/mutationParameters.h"
+#include "mutator/programMutator.h"
+#include "mutator/rng.h"
+#include "mutator/tpgMutator.h"
+
+TPG::TPGVertex& Mutator::BranchMutator::copyBranch(TPG::TPGGraph& originalGraph,
+                                        TPG::TPGGraph& targetGraph   )
+{
+    // Map to keep track of the correspondence between original and copied
+    // vertices
+    std::unordered_map<const TPG::TPGVertex*, TPG::TPGVertex*> vertexMap;
+
+    // Copy the root team and its outgoing edges
+    copyTeamAndEdges(originalGraph, targetGraph, vertexMap);
+
+    //return the rootTeam of the copied branch
+    return originalGraph.getRootVertices().at(0);
+}
+
+
+void Mutator::BranchMutator::copyTeamAndEdges(
+    TPG::TPGGraph& originalGraph, TPG::TPGGraph& targetGraph,
+    std::unordered_map<const TPG::TPGVertex*, TPG::TPGVertex*>& vertexMap)
+{
+    //Copy originalGraph's TPGVertex into targetGraph 
+    for (auto originalTeam : originalGraph.getVertices()) {
+        if (targetGraph.hasVertex(*originalTeam) == false) {
+            TPG::TPGVertex* copiedTeam;
+            // Create a new Vertex (Team/Action)
+            // (at the end of the vertices list)
+            if (dynamic_cast<const TPG::TPGTeam*>(originalTeam) != nullptr) {
+                    copiedTeam = &targetGraph.addNewTeam();
+            }
+            else if (dynamic_cast<const TPG::TPGAction*>(originalTeam) !=
+                     nullptr) {
+                    copiedTeam = &targetGraph.addNewAction(
+                        ((TPG::TPGAction*)originalTeam)->getActionID());
+            }
+
+            vertexMap[originalTeam] = copiedTeam;
+        }else {
+            vertexMap[originalTeam] = originalTeam;
+        }
+    }
+
+    // Copy originalGraph's TPGEdges into targetGraph 
+    for (auto originalEdge : originalGraph.getEdges())         
+        copyEdge(originalEdge, targetGraph, vertexMap);
+
+}
+
+
+void Mutator::BranchMutator::copyEdge(
+    std::unique_ptr<TPG::TPGEdge> originalEdge, TPG::TPGGraph& targetGraph,
+    std::unordered_map<const TPG::TPGVertex*, TPG::TPGVertex*>& vertexMap)
+{
+    // Copy the destination team/action
+    const TPG::TPGVertex* originalDestination = originalEdge.getDestination();
+    TPG::TPGVertex* copiedDestination = vertexMap[originalDestination];
+
+    // Copy the program
+    const Program::Program& originalProgram = originalEdge.getProgram();
+    Program::Program copiedProgram= Program(originalProgram);
+
+    // Create the new edge
+    targetGraph.addNewEdge(*vertexMap[&originalEdge.getSource()],
+                           *copiedDestination, copiedProgram);
+}
+
+
+void Mutator::BranchMutator::mutateBranch(const Mutator::MutationParameters& params,
+                                 Mutator::RNG& rng)
+{
+    // Perform mutations on the copied branch
+    // You can use methods from the TPGMutator class or define new mutations
+    // based on your requirements.
+    Mutator::TPGMutator::mutateTPGTeam(copiedGraph, Archive(), rootTeam, {}, {},
+                                       {}, {}, params, rng);
+    // Additional mutations as needed...
+}
+void Mutator::BranchMutator::mutateBranch(
+    TPG::TPGGraph& graph, const Archive& archive, const TPG::TPGTeam& root,
+    const std::vector<const TPG::TPGTeam*>& preExistingTeams,
+    const std::vector<const TPG::TPGAction*>& preExistingActions,
+    const std::list<const TPG::TPGEdge*>& preExistingEdges,
+    std::list<std::shared_ptr<Program::Program>>& newPrograms,
+    const Mutator::MutationParameters& params, Mutator::RNG& rng)
+{
+    
+}
